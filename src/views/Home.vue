81c3b318d3bda1c31d1e32ee00ce176b67204970
miniring <dreamhnt89@gmail.com>
2019-07-16 16:51:24 +0900

    project setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <span>count is {{ count }}</span>
+    <span>plusOne is {{ plusOne }}</span>
+    <button @click="increment">count++</button>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Vue from 'vue'
+import { value, computed, watch, onMounted } from 'vue-function-api'
 
-export default Vue.extend({
-  name: "home",
-  components: {
-    HelloWorld
+export default {
+  name: 'home',
+  setup() {
+    // reactive state
+    const count = value(0)
+    // computed state
+    const plusOne = computed(() => count.value + 1)
+    // method
+    const increment = () => {
+      count.value++
+    }
+    // watch
+    watch(
+      () => count.value * 2,
+      val => {
+        console.log(`count * 2 is ${val}`)
+      }
+    )
+    // lifecycle
+    onMounted(() => {
+      console.log(`mounted`)
+    })
+    // expose bindings on render context
+    return {
+      count,
+      plusOne,
+      increment
+    }
   }
-});
+}
 </script>
