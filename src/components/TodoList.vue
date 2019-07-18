7c31cd12414e51b706a54ec91c391ac293ca24e0
miniring <dreamhnt89@gmail.com>
2019-07-18 17:05:25 +0900

    apollo todoList 생성

---
+++ src/components/TodoList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <ApolloQuery :query="require('../graphql/TodoList.gql')">
+      <template v-slot="{ result: { data } }">
+        <div v-if="data">
+          <div v-for="todo in data.todoList" :key="todo.id">
+            {{ todo.text }}
+          </div>
+        </div>
+      </template>
+    </ApolloQuery>
+  </div>
+</template>
+
+<script lang="ts">
+import { value, computed, watch, onMounted } from 'vue-function-api'
+export default {
+  setup() {
+    console.log('todo')
+  }
+}
+</script>
+
+<style scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,44 +1,16 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <span>count is {{ count }}</span>
-    <span>plusOne is {{ plusOne }}</span>
-    <button @click="increment">count++</button>
+    <todo-list></todo-list>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import { value, computed, watch, onMounted } from 'vue-function-api'
 
 export default {
-  name: 'home',
-  setup() {
-    // reactive state
-    const count = value(0)
-    // computed state
-    const plusOne = computed(() => count.value + 1)
-    // method
-    const increment = () => {
-      count.value++
-    }
-    // watch
-    watch(
-      () => count.value * 2,
-      val => {
-        console.log(`count * 2 is ${val}`)
-      }
-    )
-    // lifecycle
-    onMounted(() => {
-      console.log(`mounted`)
-    })
-    // expose bindings on render context
-    return {
-      count,
-      plusOne,
-      increment
-    }
+  components: {
+    'todo-list': () => import('../components/TodoList.vue')
   }
 }
 </script>
